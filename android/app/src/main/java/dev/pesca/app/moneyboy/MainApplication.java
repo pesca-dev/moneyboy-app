2a423b0cdfb161dff6ca370582d63d2d74387f1d
fwcd <fwcdmail@gmail.com>
2021-08-14 22:25:35 +0200

    Update the Android package to dev.pesca.app.moneyboy

--- android/app/src/main/java/dev/pesca/app/moneyboy/MainApplication.java
+++ android/app/src/main/java/dev/pesca/app/moneyboy/MainApplication.java
@@ -1,4 +1,4 @@
-package com.moneyboy;
+package dev.pesca.app.moneyboy;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.moneyboy.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("dev.pesca.app.moneyboy.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
