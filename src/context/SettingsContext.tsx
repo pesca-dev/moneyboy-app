a8aeb2d47402c0cbfb9eff73515dbfd9de84c216
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-01-02 19:36:29 +0100

    Lint: Update SettingsContext.tsx

--- src/context/SettingsContext.tsx
+++ src/context/SettingsContext.tsx
@@ -1,11 +1,11 @@
 import React, { createContext, PropsWithChildren } from 'react';
 
-type SettingsContextType = {};
+type SettingsContextType = unknown;
 
 export const SettingsContext = createContext<SettingsContextType>({});
 
-type SettingsContextProviderProps = {};
+type SettingsContextProviderProps = unknown;
 
-export const SettingsContextProvider: React.FC<PropsWithChildren<SettingsContextProviderProps>> = ({ children }) => {
-  return <SettingsContext.Provider value={{}}>{children}</SettingsContext.Provider>;
-};
+export const SettingsContextProvider: React.FC<PropsWithChildren<SettingsContextProviderProps>> = ({ children }) => (
+  <SettingsContext.Provider value={{}}>{children}</SettingsContext.Provider>
+);
