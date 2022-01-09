69d407affff8b98c7446e19f06109bb615424030
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-01-09 17:33:25 +0100

    Structure: Extract styles for flyout header

--- src/components/general/flyouts/FlyoutHeader/FlyoutHeader.component.tsx
+++ src/components/general/flyouts/FlyoutHeader/FlyoutHeader.component.tsx
@@ -1,24 +1,13 @@
-import { useStyle } from '@moneyboy/hooks/useStyle';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { useFlyoutHeaderStyles } from './FlyoutHeader.styles';
 
 type FlyoutHeaderProps = {
   heading?: string;
 };
 
 export const FlyoutHeader: React.FC<FlyoutHeaderProps> = ({ heading }) => {
-  const { Flyouts } = useStyle();
-  const styles = StyleSheet.create({
-    flyoutHeadingContainer: {
-      width: '100%',
-      height: 30,
-    },
-    flyoutHeadingLabel: {
-      color: Flyouts.heading.color,
-      fontSize: Flyouts.heading.fontSize,
-      fontWeight: 'bold',
-    },
-  });
+  const styles = useFlyoutHeaderStyles();
   return (
     <View style={[styles.flyoutHeadingContainer]}>
       <Text style={[styles.flyoutHeadingLabel]}>{heading}</Text>
