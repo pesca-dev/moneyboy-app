50d202f55a5d9ab7cab075c10cec1de44954e8fb
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-01-09 16:25:42 +0100

    Structure: Restructure menus

--- src/components/general/menus/createPescaMenu.tsx
+++ src/components/general/menus/createPescaMenu.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { PescaMenuContextType } from '@moneyboy/api/PescaMenuContextType';
-import { createPescaMenuItem } from '@moneyboy/components/general/menus/menuItem';
-import { createPescaMenuContainer } from '@moneyboy/components/general/menus/pescaMenu';
-import { createContext } from 'react';
+import { PescaMenuContextType } from "@moneyboy/api/PescaMenuContextType";
+import { createPescaMenuItem } from "@moneyboy/components/general/menus/MenuItem";
+import { createPescaMenuContainer } from "@moneyboy/components/general/menus/PescaMenu";
+import { createContext } from "react";
 
 /**
  * Create a PescaMenu.
