1fab1a914c3379d1969db38cab47a47bf191b0e1
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-02-13 23:28:35 +0100

    Add type for PaymentContext in API directory

--- src/contexts/paymentContext/payment.context.tsx
+++ src/contexts/paymentContext/payment.context.tsx
@@ -1,6 +1,7 @@
+import { PaymentContextType } from '@moneyboy/api/PaymentContextType';
 import React, { createContext, PropsWithChildren } from 'react';
 
-export const PaymentContext = createContext({});
+export const PaymentContext = createContext<PaymentContextType>({});
 
 type PaymentContextProviderProps = unknown;
 
