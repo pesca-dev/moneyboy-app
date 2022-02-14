4991b74eae3933f07ee0fed068db9b346b5db0ca
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-02-14 17:20:40 +0100

    Store payments in PaymentContext in storage

--- src/contexts/paymentContext/payment.context.tsx
+++ src/contexts/paymentContext/payment.context.tsx
@@ -1,5 +1,6 @@
 import { PaymentContextType } from '@moneyboy/api/PaymentContextType';
 import { usePesca } from '@moneyboy/hooks/usePesca';
+import { useStorage } from '@moneyboy/hooks/useStorage';
 import React, { createContext, PropsWithChildren, useCallback, useEffect, useState } from 'react';
 
 export const PaymentContext = createContext<PaymentContextType | null>(null);
@@ -10,20 +11,27 @@ export const PaymentContextProvider: React.FC<PropsWithChildren<PaymentContextPr
   const {
     payments: { create, getAll },
   } = usePesca();
+  const [storagePayments, setStoragePayments] = useStorage('payments');
   const [payments, updatePayments] = useState<Pesca.PaymentInformation[]>([]);
 
   const update = useCallback(async () => {
-    // fetch all payments and update
+    // fetch all payments and update payments in storage
     await getAll().then(fetchedPayments => {
       if (fetchedPayments) {
-        updatePayments(fetchedPayments);
+        setStoragePayments(fetchedPayments);
       }
     });
-  }, [getAll]);
+  }, [getAll, setStoragePayments]);
 
+  // initially, we update the payments, that are stored
   useEffect(() => {
     update();
   }, [update]);
+
+  // when payments in storage change, update payments in state
+  useEffect(() => {
+    updatePayments(storagePayments);
+  }, [storagePayments, updatePayments]);
 
   // simply forward to pesca client
   const createPayment = (payment: Pesca.PaymentCreateDTO) => create(payment);
