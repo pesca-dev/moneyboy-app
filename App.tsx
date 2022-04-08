2699aca90fba364603b9ffd4bc58b4fe8baa9666
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-04-08 16:45:01 +0200

    Remove testing code for notifications

--- App.tsx
+++ App.tsx
@@ -8,10 +8,8 @@ import { SettingsContextProvider } from '@moneyboy/contexts/settingsContext';
 import { StorageContextProvider } from '@moneyboy/contexts/storageContext';
 import { StyleContextProvider } from '@moneyboy/contexts/styleContext';
 import { AppContainer } from '@moneyboy/screens/AppContainer';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar } from 'react-native';
-import { isEmulatorSync } from 'react-native-device-info';
-import { Notification, Notifications, Registered, RegistrationError } from 'react-native-notifications';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-unused-vars
@@ -19,50 +17,25 @@ declare const global: { HermesInternal: null | {} };
 
 type AppProps = unknown;
 
-export const App: React.FC<AppProps> = () => {
-  useEffect(() => {
-    if (!isEmulatorSync()) {
-      Notifications.registerRemoteNotifications();
-
-      Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
-        console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-        completion({ alert: false, sound: false, badge: false });
-      });
-
-      Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
-        console.log(`Notification opened: ${notification.payload}`);
-        completion();
-      });
-
-      Notifications.events().registerRemoteNotificationsRegistered((event: Registered) => {
-        // TODO: Send the token to my server so it could send back push notifications...
-        console.log('Device Token Received', event.deviceToken);
-      });
-      Notifications.events().registerRemoteNotificationsRegistrationFailed((event: RegistrationError) => {
-        console.error(event);
-      });
-    }
-  }, []);
-  return (
-    <SafeAreaProvider>
-      <StatusBar />
-      <StorageContextProvider>
-        <SecureStorageContextProvider>
-          <SettingsContextProvider>
-            <NotificationContextProvider>
-              <PescaContextProvider>
-                <AuthContextProvider>
-                  <PaymentContextProvider>
-                    <StyleContextProvider>
-                      <AppContainer />
-                    </StyleContextProvider>
-                  </PaymentContextProvider>
-                </AuthContextProvider>
-              </PescaContextProvider>
-            </NotificationContextProvider>
-          </SettingsContextProvider>
-        </SecureStorageContextProvider>
-      </StorageContextProvider>
-    </SafeAreaProvider>
-  );
-};
+export const App: React.FC<AppProps> = () => (
+  <SafeAreaProvider>
+    <StatusBar />
+    <StorageContextProvider>
+      <SecureStorageContextProvider>
+        <SettingsContextProvider>
+          <NotificationContextProvider>
+            <PescaContextProvider>
+              <AuthContextProvider>
+                <PaymentContextProvider>
+                  <StyleContextProvider>
+                    <AppContainer />
+                  </StyleContextProvider>
+                </PaymentContextProvider>
+              </AuthContextProvider>
+            </PescaContextProvider>
+          </NotificationContextProvider>
+        </SettingsContextProvider>
+      </SecureStorageContextProvider>
+    </StorageContextProvider>
+  </SafeAreaProvider>
+);
