cd021e78655adb339074b35fac7afb5ad25b28cb
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-04-08 16:25:46 +0200

    Remove old notification files

--- src/api/NotificationService.d.ts
+++
@@ -1,6 +0,0 @@
-// import { NotificationToken } from '@moneyboy/api/NotificationToken';
-
-// export type NotificationService = {
-//   token: NotificationToken;
-//   configure(): Promise<void>;
-// };
--- src/api/NotificationToken.d.ts
+++
@@ -1,1 +0,0 @@
-// export type NotificationToken = { os: string; token: string };
