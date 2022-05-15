577284e218bfb0e6145777999259421b214ca70b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-05-15 23:36:43 +0200

    Adjust test description

--- src/reducers/mainScreenPayments/mainScreenPayments.reducer.spec.ts
+++ src/reducers/mainScreenPayments/mainScreenPayments.reducer.spec.ts
@@ -1,7 +1,7 @@
 import { mainScreenPaymentsReducer } from '@moneyboy/reducers/mainScreenPayments/mainScreenPayments.reducer';
 
 describe('mainScreenPaymentsReducer', () => {
-  it('should return an empty object on empty input', () => {
+  it('should return an empty array on empty input', () => {
     expect(mainScreenPaymentsReducer([], { payments: [] })).toStrictEqual([]);
   });
 });
